11decb813a56947c761729ac6ff9cb29bda7fb13
mayankdaruka <53444658+mayankdaruka@users.noreply.github.com>
2020-05-11 17:39:35 -0500

    Database operations optimization

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   Future<List<UserModel>> _users;
 
   void initState() {
@@ -53,32 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
     _users = getUsers();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-    // if (_counter == 4) {
-    //   showDialog(
-    //     context: context,
-    //     builder: (context) => AlertDialog(
-    //         title: Text("Testing Alert"),
-    //         content: Text("You have pressed the button 4 times."),
-    //       )
-    //   );
-    // }
-  }
-
-  // List<Text> listUsers() {
-  //   for(var)
-  // }
-
-  void manageNewUser() {
-    newUser();
+  void manageNewUser() async {
+    await newUser();
     setState(() {
       _users = getUsers();
     });
@@ -86,16 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       // body: Center(
@@ -120,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: () => manageNewUser(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
