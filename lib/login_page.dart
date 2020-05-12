29d6eda32b444dd94d8347f00720cec7566dcecd
mayankdaruka <53444658+mayankdaruka@users.noreply.github.com>
2020-05-12 01:38:43 -0500

    Implementation of scrollable

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,13 +4,14 @@ const IP_ADDRESS = "10.0.2.2:3000";
 
 class LoginPage extends StatelessWidget {
 
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  // final TextEditingController _usernameController = TextEditingController();
+  // final TextEditingController _passwordController = TextEditingController();
 
   final emailField = TextField(
+    // controller: _usernameController,
     decoration: InputDecoration(
-      
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      prefixIcon: Icon(Icons.email),
+      // contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
       labelText: "Email",
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(32.0)
@@ -32,47 +33,38 @@ class LoginPage extends StatelessWidget {
               ),
             ) ,
           ),
-          Column(
-            children: <Widget>[
-              SizedBox(
-                height: 90.0,
-              ),
-              Container(
-                constraints: BoxConstraints.expand(height: 200.0),
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              SizedBox(
-                height: 60.0,
-              ),
-              emailField,
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password"
-                )
-              ),
-            ]
-          ),
+          Center(
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  height: 90.0,
+                ),
+                Container(
+                  constraints: BoxConstraints.expand(height: 200.0),
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                SizedBox(
+                  height: 60.0,
+                ),
+                emailField,
+                TextField(
+                  // controller: _passwordController,
+                  autofocus: true,
+                  obscureText: true,
+                  maxLength: 20,
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                    labelText: "Password"
+                  ),
+                ),
+              ]
+            ),   
+          )     
         ],)
     );
-    //   body: Column(
-    //     // mainAxisAlignment: MainAxisAlignment.center,
-    //     // crossAxisAlignment: CrossAxisAlignment.center,
-    //     children: <Widget>[
-    //       Container(
-    //         constraints: BoxConstraints.expand(height: 200.0),
-    //         child: Image.asset('assets/images/logo.png'),
-    //         padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-    //       ),
-    //       emailField,
-    //       TextField(
-    //         controller: _passwordController,
-    //         obscureText: true,
-    //         decoration: InputDecoration(
-    //           border: OutlineInputBorder(),
-    //           labelText: "Password"))]),
-    // );
   }
 }
