edb2f178e9e3717ba7644fa9a8018decc60450f1
mayankdaruka <53444658+mayankdaruka@users.noreply.github.com>
2020-05-12 02:40:36 -0500

    Front screen changes

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,6 +16,8 @@ class LoginPage extends StatelessWidget {
         children: <Widget>[
           Container(
             decoration: BoxDecoration(
+              // color: Color.fromRGBO(216, 253, 255, 1.0)
+              // color: Colors.white,
               image: DecorationImage(
                 image: AssetImage("assets/images/books.jpg"),
                 fit: BoxFit.cover,
@@ -27,45 +29,53 @@ class LoginPage extends StatelessWidget {
             child: ListView(
               children: <Widget>[
                 SizedBox(
-                  height: 60.0,
+                  height: 80.0,
                 ),
                 Container(
-                  constraints: BoxConstraints.expand(height: 170.0),
+                  constraints: BoxConstraints.expand(height: 230.0),
                   child: Image.asset('assets/images/logo.png'),
                 ),
                 SizedBox(
-                  height: 60.0,
+                  height: 80.0,
                 ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 5.0),
-                  child: TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.email),
-                      // contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                      labelText: "Email",
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red),
-                        borderRadius: BorderRadius.circular(15.0),
-                      )
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 0.0),
-                  child: TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    maxLength: 20,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0)
-                      ),
-                      labelText: "Password"
-                    ),
-                  ),
-                ), 
+                SizedBox(
+                  height: 300.0,
+                  child: Container(
+                    // color: Color.fromRGBO(216, 253, 255, 1.0)
+                    color: Colors.white54
+                  ), 
+                )
+                // Container(
+                //   padding: const EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 5.0),
+                //   child: TextFormField(
+                //     controller: _usernameController,
+                //     decoration: InputDecoration(
+                //       prefixIcon: Icon(Icons.email),
+                //       // contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                //       labelText: "Email",
+                //       enabledBorder: OutlineInputBorder(
+                //         borderSide: BorderSide(color: Colors.black, width: 1.7),
+                //         borderRadius: BorderRadius.circular(15.0),
+                //       )
+                //     ),
+                //   ),
+                // ),
+                // Container(
+                //   padding: const EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 0.0),
+                //   child: TextField(
+                //     controller: _passwordController,
+                //     obscureText: true,
+                //     maxLength: 20,
+                //     decoration: InputDecoration(
+                //       prefixIcon: Icon(Icons.lock),
+                //       enabledBorder: OutlineInputBorder(
+                //         borderSide: BorderSide(color: Colors.black, width: 1.7),
+                //         borderRadius: BorderRadius.circular(15.0),
+                //       ),
+                //       labelText: "Password"
+                //     ),
+                //   ),
+                // ), 
               ]
             ),   
           )     
