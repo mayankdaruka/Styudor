d40679bcbbec9ab1a6df51caadd3ec2a8d0f3750
mayankdaruka <53444658+mayankdaruka@users.noreply.github.com>
2020-05-14 20:31:46 -0500

    New user page implementation

--- lib/newuser_info.dart
+++ lib/newuser_info.dart
@@ -1,2 +1,33 @@
 import 'package:flutter/material.dart';
 import 'api.dart';
+
+class NewUserInfo extends StatefulWidget {
+  @override
+  _NewUserInfoState createState() => _NewUserInfoState();
+}
+
+class _NewUserInfoState extends State<NewUserInfo> {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _resumeController = TextEditingController();
+  final _profilePicController = TextEditingController();
+  final _satController = TextEditingController();
+  final _actController = TextEditingController();
+  final _mcatController = TextEditingController();
+  final _gmatController = TextEditingController();
+  final _lsatController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
